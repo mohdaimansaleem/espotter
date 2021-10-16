1ecf47b6799f92fb8b867e1bc94b0ae2d5c1b63a
MOHD AIMAN SALEEM <81529934+mohdaimansaleem@users.noreply.github.com>
2021-10-16 09:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 function clicked() {
-    let room = 0;
-    room += parseInt(document.getElementById("search").value);
+    let room = document.getElementById("search").value;
 
-    console.log(room)
     document.getElementById("modal").style.display = "flex";
     document.getElementById("main").style.display = "none";
-    if (room) {
-        document.getElementById("h3").innerText += "\n yesss"
+
+    if (document.getElementById("search").value == "") {
+        document.getElementById("h3").innerText = "ENTER A ROOM NUMBER..."
+        return;
+    }
+
+
+
+    if (room.length >= 4) {
+        document.getElementById("h3").innerText = "\n yesss"
     }
 
     else {
-        document.getElementById("h3").innerText += "\n nooooo"
+        document.getElementById("h3").innerText = room;
     }
+}
+
+function unclicked() {
+
+
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("main").style.display = "flex";
+
 }
