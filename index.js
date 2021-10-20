603263c1b751bb788f3e65eebbb8d4dba592aebd
MOHD AIMAN SALEEM <81529934+mohdaimansaleem@users.noreply.github.com>
2021-10-20 12:14:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
-var map = L.map('map').setView([17.454704, 78.666414], 17);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-let marker = L.marker([17.454704, 78.666414]).addTo(map);
+var a=0,b=0;
 
 function clicked() {
-    let room = document.getElementById("search").value;
+  let roomNumber=document.getElementById("search").value;
+  
+  var coordinates={
+    "1001":{
+      "lat":17.455249973437514,
+      "long":78.6665028333664,
+    },
+    "1002":{
+      "lat":17.45657025649771,
+      "long":78.66678982973099,
+      
+    },
+  
+    "1003":{
+      "lat": 17.457140840983552,
+      "long":78.66740942001343,
+    },
+ 
+   "2001":{
+    "lat":17.454162523809906,
+    "long": 78.6665403842926,
+  },
+  "2002":{
+    "lat":17.45692079516033,
+    "long":78.6670634150505,
 
-    document.getElementById("modal").style.display = "flex";
-    document.getElementById("main").style.display = "none";
+  },
 
-    if (document.getElementById("search").value == "") {
-        document.getElementById("h3").innerText = "ENTER A ROOM NUMBER..."
-        return;
-    }
+ 
+
+ };
+
+ a=coordinates[roomNumber]["lat"];
+ b=coordinates[roomNumber]["long"];
 
 
+ localStorage.setItem("lat",a);
+ localStorage.setItem("long",b);
 
-    if (room.length >= 4) {
-        document.getElementById("h3").innerText = "\n yesss"
-    }
-
-    else {
-        document.getElementById("h3").innerText = room;
-    }
 }
 
-function unclicked() {
+
+let c=localStorage.getItem('lat');
+let d=localStorage.getItem('long');
+  var map = L.map('map').setView([17.454704, 78.666414], 100);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
 
 
-    document.getElementById("modal").style.display = "none";
-    document.getElementById("main").style.display = "flex";
-
-}
-
-
-
-
-
+   L.marker([c,d]).addTo(map)
+   localStorage.clear();
