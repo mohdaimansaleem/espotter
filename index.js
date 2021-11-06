be5f9be6ac9bde8d4b44a5db1742aa7ab253edce
MOHD AIMAN SALEEM <81529934+mohdaimansaleem@users.noreply.github.com>
2021-11-06 17:19:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,41 @@ function clicked() {
  
 
   let roomNumber = document.getElementById("search").value;
+  
+  var sNumber=roomNumber; 
+  roomNumber=parseInt(roomNumber);
+  console.log(roomNumber)
+  console.log(sNumber)
+  
   var lat = 0;
   var lon = 0;
 
 
  
 
-  if (roomNumber.startsWith("11") || roomNumber.startsWith("12") || roomNumber.startsWith("13") || roomNumber.startsWith("14")) {
-    if (parseInt(roomNumber) >= 1101 && parseInt(roomNumber) <= 1114 || parseInt(roomNumber) >= 1201 && parseInt(roomNumber) <= 2214 || parseInt(roomNumber) >= 1301 && parseInt(roomNumber) <=1320 || parseInt(roomNumber) >= 1401 && parseInt(roomNumber) <= 1415) {
-      roomNumber = "1001"
+  if (sNumber.startsWith("11") || sNumber.startsWith("12") || sNumber.startsWith("13") || sNumber.startsWith("14")) {
+    if (roomNumber >= 1101 && roomNumber <= 1114 || roomNumber >= 1201 && roomNumber <= 2214 || roomNumber >= 1301 && roomNumber <=1320 || roomNumber >= 1401 && roomNumber <= 1415) {
+      sNumber = "1001"
     }
     //ECE BLOCK
   }
 
-  if (roomNumber.startsWith("21") || roomNumber.startsWith("22") || roomNumber.startsWith("23") || roomNumber.startsWith("24")) {
-    if (parseInt(roomNumber) >= 2101 && parseInt(roomNumber) <= 2112 || parseInt(roomNumber) >= 2201 && parseInt(roomNumber) <= 2215 || parseInt(roomNumber) >= 2301 && parseInt(roomNumber) <= 2311 || parseInt(roomNumber) >= 2401 && parseInt(roomNumber) <= 2414) {
-      roomNumber = "1002"
+  if (sNumber.startsWith("21") || sNumber.startsWith("22") || sNumber.startsWith("23") || sNumber.startsWith("24")) {
+    if (roomNumber >= 2101 && roomNumber <= 2112 || roomNumber >= 2201 && roomNumber <= 2215 || roomNumber >= 2301 && roomNumber <= 2311 || roomNumber >= 2401 && roomNumber <= 2414) {
+      sNumber = "1002"
     }
   //IT BLOCK
   }
-  if (roomNumber.startsWith("42") || roomNumber.startsWith("43") || roomNumber.startsWith("44") ) {
-    if (parseInt(roomNumber) >= 4201 && parseInt(roomNumber) <= 4215 || parseInt(roomNumber) >= 4301 && parseInt(roomNumber) <= 4315 || parseInt(roomNumber) >= 4401 && parseInt(roomNumber) <=4415) {
-      roomNumber = "1003"
+  if (sNumber.startsWith("42") || sNumber.startsWith("43") || sNumber.startsWith("44") ) {
+    if (roomNumber >= 4201 && roomNumber <= 4215 || roomNumber >= 4301 && roomNumber <= 4315 || roomNumber >= 4401 && roomNumber <=4415) {
+      sNumber = "1003"
     }
      //FIRST YEAR BLOCK
   }
 
-  if (roomNumber.startsWith("81") || roomNumber.startsWith("82") || roomNumber.startsWith("83")|| roomNumber.startsWith("84")) {
-    if (parseInt(roomNumber) >= 8101 && parseInt(roomNumber) <= 8107 || parseInt(roomNumber) >= 8201 && parseInt(roomNumber) <= 8207 || parseInt(roomNumber) >= 8301 && parseInt(roomNumber) <=8307|| parseInt(roomNumber) >= 8401 && parseInt(roomNumber) <=8407) {
-      roomNumber = "1004"
+  if (sNumber.startsWith("81") || sNumber.startsWith("82") || sNumber.startsWith("83")|| sNumber.startsWith("84")) {
+    if (roomNumber >= 8101 && roomNumber <= 8107 || roomNumber >= 8201 && roomNumber <= 8207 || roomNumber >= 8301 && roomNumber <=8307|| roomNumber >= 8401 && roomNumber <=8407) {
+      sNumber = "1004"
     }
   // BIOTECH BLOCK
   }
@@ -64,8 +70,8 @@ function clicked() {
 
   };
 
-  lat = coordinates[roomNumber]["lat"];
-  lon = coordinates[roomNumber]["long"];
+  lat = coordinates[sNumber]["lat"];
+  lon = coordinates[sNumber]["long"];
 
   var map = L.map('map').setView([17.454704, 78.666414], 17);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
